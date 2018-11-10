fb613c03b1b0e97947b5d426c34afde3497fc58f
imanalopher <iman.mamadiyev@gmail.com>
2018-11-11 03:17:29 +0600

    fonts awesome & header logo center and social icons

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import popper from 'popper.js/dist/umd/popper'
 // import bootstrap from 'bootstrap/dist/js/bootstrap'
 import bootstrapMd from 'bootstrap-material-design/dist/js/bootstrap-material-design'
+import fontawsome from '@fortawesome/fontawesome-free/js/all.min.js';
 
 $(() => {
   $("body").bootstrapMaterialDesign();
