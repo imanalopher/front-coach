1ef7ac779dc5c864ac5192d291608a0fcfd32ef6
imanalopher <iman.mamadiyev@gmail.com>
2018-11-12 02:00:23 +0600

    Form is ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,31 @@
-import $ from 'jquery';
-import popper from 'popper.js/dist/umd/popper'
-// import bootstrap from 'bootstrap/dist/js/bootstrap'
-import bootstrapMd from 'bootstrap-material-design/dist/js/bootstrap-material-design'
-import fontawsome from '@fortawesome/fontawesome-free/js/all.min.js';
-import jcf from 'jcf/js/jcf';
+import $ from "jquery";
+import popper from "popper.js/dist/umd/popper";
+import bootstrapMd from "bootstrap-material-design/dist/js/bootstrap-material-design";
+
+import fontawsome from "@fortawesome/fontawesome-free/js/all";
+import jcf from "jcf/js/jcf";
+import jcfSelect from "jcf/js/jcf.select";
 
 $(() => {
   $("body").bootstrapMaterialDesign();
 
-  function headerFix() {
-    if ($(window).scrollTop() > 0) {
-      $("header").addClass('fixed');
-    }
-    else {
-      $("header").removeClass('fixed');
-    }
-  }
-
-  headerFix();
-  $(window).scroll(() => headerFix());
-
-  jcf.setOptions('Checkbox', {
+  jcf.setOptions("Select", {
     wrapNative: false,
     wrapNativeOnMobile: false,
-    useCustomScroll: true,
+    useCustomScroll: true
   });
-
   jcf.replaceAll();
+
+  // --- Add Or Remove Header Fixed Class ---
+  function headerFix() {
+    if ($(window).scrollTop() > 0) {
+      $("header").addClass("fixed");
+    } else {
+      $("header").removeClass("fixed");
+    }
+  }
+  headerFix();
+  $(window).scroll(() => {
+    headerFix();
+  });
 });
