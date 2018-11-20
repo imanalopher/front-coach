917c0a81cc22d0ee36e9f84679b7cb4737992c31
imanalopher <iman.mamadiyev@gmail.com>
2018-11-20 23:06:54 +0600

    Coach list page Google Map is fixed on the side bar

--- src/js/coach-list.js
+++ src/js/coach-list.js
@@ -1,2 +1,10 @@
 import infoBubble from 'js-info-bubble';
 
+import stickySideBar from 'sticky-sidebar';
+
+var sticky = new StickySidebar('#coach-list-map-wrap', {
+  containerSelector: '#coach-list-map',
+  innerWrapperSelector: '#coach-list-map',
+  topSpacing: 20,
+  bottomSpacing: 20
+});
