db95578fc3cb45a0cacc9331a0e19081fc6b9c36
imanalopher <iman.mamadiyev@gmail.com>
2018-11-25 19:45:11 +0600

    Coach profile - Google map & sections are done

---
+++ src/js/coach-profile.js
@@ -0,0 +1,20 @@
+import airDatepicker from "air-datepicker/dist/js/datepicker";
+import airDatepickerEn from "air-datepicker/dist/js/i18n/datepicker.en";
+import stickySideBar from "sticky-sidebar";
+
+$(() => {
+  $("#booking-date").datepicker({
+    language: "en",
+    timeFormat: 'hh:mm:ii',
+    timepicker: true,
+    minDate: new Date(),
+    toggleSelected: true
+  });
+
+  let coachBookForm = new StickySidebar("#book-form-handler", {
+    containerSelector: "#coach-info",
+    innerWrapperSelector: "#book-form-handler",
+    topSpacing: 70,
+    bottomSpacing: 20
+  });
+});
