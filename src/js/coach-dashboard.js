0b42d34c0193a1c5370e4aaac9cb46d9176ce174
imanalopher <iman.mamadiyev@gmail.com>
2018-11-30 16:46:46 +0600

    Dropzone package installed & style setup

--- src/js/coach-dashboard.js
+++ src/js/coach-dashboard.js
@@ -1,1 +1,12 @@
-console.log("hello");
+import $ from "jquery";
+import Dropzone from "dropzone";
+
+$(function() {
+  var myDropzone = new Dropzone(document.body, {
+    url: '/target-url', // Set the url
+    thumbnailWidth: 180,
+    thumbnailHeight: 180,
+    parallelUploads: 120,
+    autoQueue: false, // Make sure the files aren't queued until manually added
+  });
+});
