0c9082a62426ca48b42592ee2f1f79d82a3f7888
imanalopher <iman.mamadiyev@gmail.com>
2018-12-09 13:04:06 +0600

    Rate Form

--- src/js/coach-dashboard-review.js
+++ src/js/coach-dashboard-review.js
@@ -1,1 +1,15 @@
-console.log("hello");
+
+$(() => {
+  console.log("$");
+  $(".rate-level-input").change(e => {
+    let rateLevel = e.target;
+    $(".rate-level-input").not($(rateLevel)).prop("checked", false);
+
+    let length = $(rateLevel).val();
+
+    for (let i = 1; i < length; i++) {
+      $("#rate-level-" + i).prop("checked", true);
+    }
+  });
+});
+console.log("before");
